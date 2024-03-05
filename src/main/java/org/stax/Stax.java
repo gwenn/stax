caa9afa907b0d990254492ced2c04e28f7d56006
gwenn <gtreguier@gmail.com>
2024-03-05 20:02:52 +0100

    Remove Stax#from methods

--- src/main/java/org/stax/Stax.java
+++ src/main/java/org/stax/Stax.java
@@ -2,26 +2,11 @@ package org.stax;
 
 import org.codehaus.stax2.XMLStreamReader2;
 import org.codehaus.stax2.XMLStreamWriter2;
-import org.codehaus.stax2.io.Stax2ByteArraySource;
-import org.codehaus.stax2.io.Stax2FileSource;
-import org.codehaus.stax2.io.Stax2StringSource;
 
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
-import javax.xml.transform.Source;
-import java.io.File;
 
 public interface Stax {
-    static Source from(String text) {
-        return new Stax2StringSource(text);
-    }
-    static Source from(byte[] buf) {
-        return new Stax2ByteArraySource(buf, 0, buf.length);
-    }
-    static Source from(File f) {
-        return new Stax2FileSource(f);
-    }
-
     /**
      * Copy event(s) from reader to writer
      */
